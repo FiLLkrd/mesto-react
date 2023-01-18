e2091fa8dd90df34b1227b0dbf4a7b8def606629
FiLLkrd <ma.antonoff2012@yandex.ru>
2023-01-18 23:46:57 +0300

    баг с Api в тестах

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import api from '../utils/api'
+import api from '../utils/api.js'
 import Card from './Card';
 
 export default function Main (props) {
